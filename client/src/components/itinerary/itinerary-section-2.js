dddc5bc72397e0e21263ded9bee21cb02f2a8985
Kevin Shirley <kevinxshirley@gmail.com>
2019-06-18 19:50:26 -0400

    new mac start

--- client/src/components/itinerary/itinerary-section-2.js
+++ client/src/components/itinerary/itinerary-section-2.js
@@ -16,7 +16,79 @@ class ItinerarySection2 extends Component {
 						</div>
 					</div>
 
-					<div className="itinerary-item-container">
+					<div className="destinations-container container-fluid">
+						<div className="content row">
+
+							<Fade bottom>
+								<Link to="/itinerary/ireland">
+									<div className="itinerary-item">
+										<img src={ireland} alt="Ireland" />
+										<div className="text">
+											<h3>Ireland</h3>
+										</div>
+									</div>
+								</Link>
+							</Fade>
+
+							<Fade bottom>
+								<Link to="/itinerary/spain-and-portugal">
+									<div className="itinerary-item">
+										<img src={ireland} alt="Spain and Portugal" />
+										<div className="text">
+											<h3>Spain and Portugal</h3>
+										</div>
+									</div>
+								</Link>
+							</Fade>
+
+							<Fade bottom>
+								<Link to="/itinerary/italia">
+									<div className="itinerary-item">
+										<img src={ireland} alt="Italia" />
+										<div className="text">
+											<h3>Italia</h3>
+										</div>
+									</div>
+								</Link>
+							</Fade>
+						
+							{/*<div className="itinerary-item">
+								<Link to="/itinerary/ireland">
+									<img src={ireland} alt="Ireland" />
+									<div className="overlay">
+										<div className="text">
+											<h3>Ireland</h3>
+										</div>
+									</div>
+								</Link>
+							</div>
+
+							<div className="itinerary-item">
+								<Link to="/itinerary/spain-and-portugal">
+									<img src={day1Img} alt="Spain and Portugal" />
+									<div className="overlay">
+										<div className="text">
+											<h3>Spain and Portugal</h3>
+										</div>
+									</div>
+								</Link>
+							</div>
+
+							<div className="itinerary-item">
+								<Link to="/itinerary/italia">
+									<img src={day1Img} alt="Italia" />
+									<div className="overlay">
+										<div className="text">
+											<h3>Italia</h3>
+										</div>
+									</div>
+								</Link>
+							</div>*/}
+
+						</div>
+					</div>
+
+					{/*<div className="itinerary-item-container">
 
 						<div className="itinerary-item">
 							<Link to="/itinerary/ireland">
@@ -51,7 +123,7 @@ class ItinerarySection2 extends Component {
 							</Link>
 						</div>
 
-					</div>
+					</div>*/}
 				
 				</div>
 			</section>
